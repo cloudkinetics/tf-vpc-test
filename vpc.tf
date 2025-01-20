17eed54e8d64ee8b045f2b48fcaf40cf9abc31b8
ArchanaSounder <81344540+ArchanaSounder@users.noreply.github.com>
2025-01-20 20:52:51 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,3 +3,11 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "ltc-backendtest.tf"
+    key    = "corestack/terraform.tfstate"
+    region = "ap-northeast-3"
+  }
+}
